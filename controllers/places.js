2e2db246749a1de82d554025a115d5c027481d1a
cfdebug <rfadventure22@gmail.com>
2022-09-25 13:07:25 -0500

    Added Comments

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,10 @@
+// Places Router
 const router = require('express').Router()
 
+// Route
 router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
+// Export
 module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// Required Modules/Configuration
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.use('/places', require('./controllers/places'))
 
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
@@ -12,5 +14,6 @@ app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
+// Start Listening/Open the Doors
 app.listen(process.env.PORT)
 
