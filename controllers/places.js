dbbdc6ae3822d7390f96724c41d117dc6b8acc10
cfdebug <rfadventure22@gmail.com>
2022-10-04 02:06:19 -0500

    Delete Functionality Implemented

--- controllers/places.js
+++ controllers/places.js
@@ -47,9 +47,20 @@ router.get('/:id/edit', (req, res) => {
 })
 
 // DELETE
-router.delete('/:id', (req,res) => {
-  res.redirect('/')
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
 })
+
 
 
 // Export
