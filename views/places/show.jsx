b2138b0541b114e4bf7436544375f367ba95e5cb
cfdebug <rfadventure22@gmail.com>
2022-10-11 21:13:50 -0500

    Added Helper Method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,33 +7,39 @@ function show (data) {
        <main>
        <div className="row">
               <div className="col-sm-6">
-                <img className = 'home-img' src={data.place.pic} alt={data.place.name} />
+                <img className='home-img' src={data.place.pic} alt={data.place.name} />
                 <h3>
                   Located in {data.place.city}, {data.place.state}
                 </h3>
               </div>
               <div className="col-sm-6">
-                <h1 className='text-primary'>{ data.place.name }</h1>
-                <h5>Established {data.place.founded}</h5>
-                <h2>Rating: Currently Unrated</h2>
-                </div>
-                <div className='col-sm-6'>
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                <h5>Unrated</h5>
+                <br />
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
                 <h4>
                   Serving {data.place.cuisines}
                 </h4>
-                </div>
-                <hr />
-                <h2>Comments</h2>
-                <h5>
-                    No Comments Yet!
-                </h5>
+                <br />
                 <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">Edit <i className='fas fa-pen'></i></a>  
                 <form method='POST' action={`/places/${data.place.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
                         Delete <i className='fas fa-trash-alt'></i>
                     </button>
-                </form>     
-        </div>
+                </form>
+              </div>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            <h5>No Comments Yet!</h5>
         </main> 
     </Def>
     )
