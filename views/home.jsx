16e8c4d6760e9ccac7a0ff02255ae3320b24be71
cfdebug <rfadventure22@gmail.com>
2022-10-19 19:35:13 -0500

    Add Mongo Atlas

--- views/home.jsx
+++ views/home.jsx
@@ -12,7 +12,7 @@ function home () {
                 <img className='home-img' src="/images/chinese.jpg" alt="Chinese Food" />
                 <img className='home-img' src="/images/american.jpg" alt="American Food" />
                 <div>
-                Photos by various artist on <a href="https://unsplash.com/s/photos/mexican-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash, </a><a href="https://www.chefdenise.com/all/what-to-eat-in-italy-1">Chef Denise, </a><a href='https://www.istockphoto.com/photo/chinese-food-blank-background-gm545286388-98216699?phrase=chinese%20food'>iStock, and </a><a href='https://www.pexels.com/search/american%20food/'>Pexels.</a>
+                Photos by various artists on <a href="https://unsplash.com/s/photos/mexican-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash, </a><a href="https://www.chefdenise.com/all/what-to-eat-in-italy-1">Chef Denise, </a><a href='https://www.istockphoto.com/photo/chinese-food-blank-background-gm545286388-98216699?phrase=chinese%20food'>iStock, and </a><a href='https://www.pexels.com/search/american%20food/'>Pexels.</a>
                 </div>
             </div>
             <a href="/places">
